41fb479aed12db545ad8d7f10055014ffc9218bd
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2024-06-11 23:51:28 +0600

    navigation loader & error page added & accordio added

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,36 +1,48 @@
 const Accordion = () => {
   return (
-    <>
-      <div className="join join-vertical w-full">
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
+    <div className="text-center space-y-10">
+      <span className="inline-block px-4 text-3xl font-bold border-b-rose-600 border-b-4 pb-2">
+        Faq
+      </span>
+      <div className="grid lg:grid-cols-2 gap-8 place-items-center text-left">
+        <div className="join join-vertical w-full">
+          <div className="collapse collapse-arrow join-item border border-base-300">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
           </div>
-          <div className="collapse-content">
-            <p>hello</p>
+          <div className="collapse collapse-arrow join-item border border-base-300">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+          <div className="collapse collapse-arrow join-item border border-base-300">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title text-xl font-medium">
+              Click to open this one and close others
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
           </div>
         </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content">
-            <p>hello</p>
-          </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border border-base-300">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title text-xl font-medium">
-            Click to open this one and close others
-          </div>
-          <div className="collapse-content">
-            <p>hello</p>
-          </div>
+        <div className="h-96">
+          <img
+            className="h-full"
+            src="https://i.postimg.cc/J0ctHTMG/faq.png"
+            alt=""
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Accordion;
